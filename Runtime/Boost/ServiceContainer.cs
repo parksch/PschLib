c72a7f5cddd5fb9a8b97cabf4404b41e9427f9bb
parksch <sakoedak4555@naver.com>
2026-07-23 11:10:19 +0900

    Feat ServiceContainer

--- Runtime/Boost/ServiceContainer.cs
+++ Runtime/Boost/ServiceContainer.cs
@@ -1,9 +1,47 @@
-
+using System;
+using System.Collections.Generic;
 
 namespace PschLib
 {
-    public class ServiceContainer
+    public sealed class ServiceContainer
     {
+        private readonly Dictionary<Type, object> _services = new Dictionary<Type, object>();
 
+        public bool Register<TService>(TService service)
+            where TService : class
+        {
+            if (service == null)
+            {
+                throw new ArgumentNullException(nameof(service));
+            }
+
+            var serviceType = typeof(TService);
+            if (_services.ContainsKey(serviceType))
+            {
+                return false;
+            }
+
+            _services.Add(serviceType, service);
+            return true;
+        }
+
+        public bool Unregister<TService>()
+            where TService : class
+        {
+            return _services.Remove(typeof(TService));
+        }
+
+        public TService Get<TService>()
+            where TService : class
+        {
+            object service;
+            if (!_services.TryGetValue(typeof(TService), out service))
+            {
+                throw new InvalidOperationException(
+                    $"Service {typeof(TService).FullName} is not registered.");
+            }
+
+            return (TService)service;
+        }
     }
 }
